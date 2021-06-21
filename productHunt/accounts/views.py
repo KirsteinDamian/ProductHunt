abe9c37a6cdb44aa4ac5cca8b21f84a5df014ec0
Damian Kirstein <kirsteindamian@gmail.com>
2021-06-21 16:53:51 +0200

    Login and Logout

--- productHunt/accounts/views.py
+++ productHunt/accounts/views.py
@@ -14,15 +14,26 @@ def signup(request):
                 auth.login(request, user)
                 return redirect('home')
         else:
-            return render(request, 'accounts/signup.html', {'error': 'Password must be the same as confirmed password!'})
+            return render(request, 'accounts/signup.html',
+                          {'error': 'Password must be the same as confirmed password!'})
     else:
         return render(request, 'accounts/signup.html')
 
 
 def login(request):
-    return render(request, 'accounts/login.html')
+    if request.method == 'POST':
+        user = auth.authenticate(username=request.POST['username'], password=request.POST['password'])
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'accounts/login.html',
+                          {'error': 'Username or password is incorrect!'})
+    else:
+        return render(request, 'accounts/login.html')
 
 
 def logout(request):
-    # TODO Need to route to homepage
-    return render(request, 'accounts/logout.html')
+    if request.method == 'POST':
+        auth.logout(request)
+        return redirect('home')
